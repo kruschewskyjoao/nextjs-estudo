7193d26da8531df1517179a62aaadb82d3767027
joaocerqueira <joaokruschewsky@gmail.com>
2024-02-23 17:38:39 -0300

    add nav bar more infos + css

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,24 @@
 import styles from './styles.module.css';
+import Link from 'next/link';
 
 export function Header() {
   return (
-    <div>
-      <h2>Tarefas</h2>
-    </div>
+    <header className={styles.header}>
+      <section className={styles.content}>
+        <nav className={styles.nav}>
+          <Link href="/">
+            <h2 className={styles.logo}>
+              Tarefas<span>+</span>
+            </h2>
+          </Link>
+        <Link href="/dashboad" className={styles.link}>
+          Meu painel
+        </Link>
+        </nav>
+        <button className={styles.loginButton}>
+          Acessar
+        </button>
+      </section>
+    </header>
   )
 }
